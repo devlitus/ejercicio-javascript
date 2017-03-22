20db2683f8aa78de04616aa24bef166765fdc260
devlitus <developercarles@gmail.com>
2017-03-22 20:21:01 +0100

    final del ejercicio 5

--- ejercicio5/script5.js
+++ ejercicio5/script5.js
@@ -1,26 +1,34 @@
-
-var sal = prompt("saldo");
-saldo = parseInt(sal);
-
-function calcular(saldo) {
+$(document).ready(function () {
   var sal = prompt("saldo");
-  saldo = parseInt(sal);
+  console.log(calcular(sal));
+  function calcular(saldo) {
+
+  saldo = parseInt(saldo);
    //var saldo = $("#sal").val();
-  var irpf = (saldo * 15) / 100;
-  console.log(irpf);
-  switch (true) {
-    case ((irpf > 0) && (irpf<=12450)):
-      console.log("La retención es: 19%");
-      break;
-    case ((irpf >=12451) && (irpf <= 20200)):
-      console.log("La retención es: 24%");  
-      break;
-  
-    default:
-      break;
+    // var irpf = (saldo * 15) / 100;
+    // console.log(irpf);
+    switch (true) {
+      case ((saldo > 0) && (saldo<=12450)):
+        console.log("La retención es: 19%");
+        break;
+      case ((saldo >=12451) && (saldo <= 20200)):
+        console.log("La retención es: 24%");  
+        break;
+      case ((saldo >=20201) && (saldo <= 35200)):
+        console.log("La retención es: 30%");  
+        break;
+      case ((saldo >=35201) && (saldo <= 60000)):
+        console.log("La retención es: 37%");  
+        break;
+      default:
+         console.log("La retención es: 45%");
+    }
+    
   }
-  // console.log(saldo);
-}
+  
+})
+
+
 
 
 
